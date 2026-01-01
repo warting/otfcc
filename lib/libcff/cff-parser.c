6d2c2c55f87b567a907a05fe0a4f1868e2712a93
Stefan Wärting <stefan@warting.se>
2026-01-01 11:35:56 +0100

    Implement recursion depth limit in CFF parser to prevent stack overflow

--- lib/libcff/cff-parser.c
+++ lib/libcff/cff-parser.c
@@ -341,7 +341,8 @@ static double callback_nopgetrand(void *context) {
 // CFF charastring parser
 void cff_parseOutline(uint8_t *data, uint32_t len, cff_Index gsubr, cff_Index lsubr,
                       cff_Stack *stack, void *outline, cff_IOutlineBuilder methods,
-                      const otfcc_Options *options) {
+                      const otfcc_Options *options, uint32_t depth) {
+	if (depth > 64) return;
 	uint16_t gsubr_bias = compute_subr_bias(gsubr.count);
 	uint16_t lsubr_bias = compute_subr_bias(lsubr.count);
 	uint8_t *start = data;
@@ -816,7 +817,7 @@ void cff_parseOutline(uint8_t *data, uint32_t len, cff_Index gsubr, cff_Index ls
 						cff_parseOutline(lsubr.data + lsubr.offset[lsubr_bias + subr] - 1,
 						                 lsubr.offset[lsubr_bias + subr + 1] -
 						                     lsubr.offset[lsubr_bias + subr],
-						                 gsubr, lsubr, stack, outline, methods, options);
+						                 gsubr, lsubr, stack, outline, methods, options, depth + 1);
 						break;
 					}
 					case op_callgsubr: {
@@ -825,7 +826,7 @@ void cff_parseOutline(uint8_t *data, uint32_t len, cff_Index gsubr, cff_Index ls
 						cff_parseOutline(gsubr.data + gsubr.offset[gsubr_bias + subr] - 1,
 						                 gsubr.offset[gsubr_bias + subr + 1] -
 						                     gsubr.offset[gsubr_bias + subr],
-						                 gsubr, lsubr, stack, outline, methods, options);
+						                 gsubr, lsubr, stack, outline, methods, options, depth + 1);
 						break;
 					}
 					default: {
--- lib/libcff/libcff.h
+++ lib/libcff/libcff.h
@@ -155,7 +155,7 @@ void cff_mergeCS2Special(caryll_Buffer *blob, uint8_t val);
 
 extern uint8_t cff_parseSubr(uint16_t idx, uint8_t *raw, cff_Index fdarray, cff_FDSelect select, cff_Index *subr);
 void cff_parseOutline(uint8_t *data, uint32_t len, cff_Index gsubr, cff_Index lsubr, cff_Stack *stack, void *outline,
-                      cff_IOutlineBuilder methods, const otfcc_Options *options);
+                      cff_IOutlineBuilder methods, const otfcc_Options *options, uint32_t depth);
 
 // File
 extern cff_File *cff_openStream(uint8_t *data, uint32_t len, const otfcc_Options *options);
